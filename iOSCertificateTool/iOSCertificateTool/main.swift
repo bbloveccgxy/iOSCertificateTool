aa06e9d6c3758ed5bba8fbcbe94daedd8fa4c864
gaoxinyuan <294635134@qq.com>
2018-04-24 16:11:42 +0800

    mobileprovision

--- iOSCertificateTool/iOSCertificateTool/main.swift
+++ iOSCertificateTool/iOSCertificateTool/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+import XYMobileprovisionReader
+import XYP12Reader
+
+let argsCount = CommandLine.arguments.count
+
+if argsCount > 3 || argsCount < 2 {
+    print("Wrong arguments count: \(argsCount)")
+    exit(1)
+}
+
+let filePath = CommandLine.arguments[1]
+var password : String
+
+if argsCount == 3 {
+    password = CommandLine.arguments[2]
+}
+
+
+let extentsion = NSString(string: filePath).pathExtension
+
+if extentsion == "p12" {
+    
+} else if extentsion == "mobileprovision" {
+    let mobileprovisionReader = MobileprovisionReader(path:filePath)
+    let mobileprovisionInfo = mobileprovisionReader.getInfo()
+    mobileprovisionInfo!.description()
+} else {
+    print("Wrong File Type")
+    exit(1)
+}
+
+
+
+
 
